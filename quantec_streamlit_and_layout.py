7793d34d7fb15bc7b6fdb977a6bd577c57b598c7
PR0VERB <36727013+PR0VERB@users.noreply.github.com>
2021-08-02 08:33:30 +0200

    Update quantec_streamlit_and_layout.py

--- quantec_streamlit_and_layout.py
+++ quantec_streamlit_and_layout.py
@@ -72,14 +72,14 @@ def loadNLP():
 
 nlp = loadNLP()
 
-@st.cache # from streamlit cheat sheet https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py
+# @st.cache # from streamlit cheat sheet https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py
 def dfUnmodified(): #put all the dataframe ops in cached function
     df_ = pd.read_csv('86077x278_1Jan2020.csv')
 
 
     df = df_.copy()
     df['Dates'] = pd.to_datetime(df['Dates'])
-#     df = df.sort_values('Dates')
+    df = df.sort_values('Dates')
 
     # Country Names
     countryData = pd.read_csv('countryData.csv')
